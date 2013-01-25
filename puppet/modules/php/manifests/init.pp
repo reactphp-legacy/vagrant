a82946ca996817c64f055034aaf614860f72c243
Jeremy Cook <jeremycook0@gmail.com>
2013-01-25 12:47:09 -0500

    Removing php module.

--- puppet/modules/php/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class php {
-  # Installs PHP and restarts Apache to load the module
-  package { ['php5', 'php-apc', 'libapache2-mod-php5', 'php5-dev', 'php5-xdebug', 'php5-mysql', 'php5-gd', 'php5-imagick', 'php5-mcrypt', 'php5-memcache', 'php5-mhash', 'php5-pspell', 'php5-snmp', 'php5-sqlite', 'php5-xmlrpc', 'php5-xsl']:
-    ensure  => installed,
-    notify  => Service['apache2'],
-    require => [Package['mysql-client'], Package['apache2']],
-  }
-}
