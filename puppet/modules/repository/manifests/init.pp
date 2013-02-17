9661d1b4c313fac98320fbdb72e30c1a60e28bfa
Jeremy Cook <jeremycook0@gmail.com>
2013-02-17 12:57:04 -0500

    Edits to make sure dependencies are loaded in the correct order.

--- puppet/modules/repository/manifests/init.pp
+++ puppet/modules/repository/manifests/init.pp
@@ -3,6 +3,10 @@ class repository {
   exec {"/usr/bin/add-apt-repository ppa:ondrej/php5 && /usr/bin/apt-get update":
       alias   => "php54_repository",
       require => Package["python-software-properties"],
-      before  => Package['php']
+      before  => [Class['php_modules'], Package['php'], Package['php-pear'], Package['php-devel']]
+  }
+  exec {"apt-get update":
+	  require => Exec['php54_repository'],
+      before  => [Class['php_modules'], Package['php'], Package['php-pear'], Package['php-devel']]
   }
 }
