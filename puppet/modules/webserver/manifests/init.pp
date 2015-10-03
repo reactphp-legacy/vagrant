0b5cc7f1dc59c43e3d00b8bf99fc39ce4024b28d
Jamie <hubdotcom@users.noreply.github.com>
2015-10-03 12:20:35 +0200

    Should this be example.dev?

--- puppet/modules/webserver/manifests/init.pp
+++ puppet/modules/webserver/manifests/init.pp
@@ -1,7 +1,7 @@
 # Setups a default virtual host that serves a phpinfo page,
 class webserver {
-  file { '/etc/apache2/sites-enabled/example.com':
-    path    => '/etc/apache2/sites-enabled/example.com',
+  file { '/etc/apache2/sites-enabled/example.dev':
+    path    => '/etc/apache2/sites-enabled/example.dev',
     source  => 'puppet:///modules/webserver/site.conf',
     notify  => Service['apache2'],
     require => Package['apache2'],
